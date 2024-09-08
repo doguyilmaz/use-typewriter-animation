0349d000d2b5f4464da9100808b9db347893ac35
Dogu Yilmaz <dogukyilmaz@gmail.com>
2024-09-08 23:33:57 +0300

    migrate js to ts

--- esbuild.ts
+++ esbuild.ts
@@ -1,8 +1,8 @@
-const esbuild = require('esbuild');
-const { nodeExternalsPlugin } = require('esbuild-node-externals');
+import { build, BuildOptions } from 'esbuild';
+import { nodeExternalsPlugin } from 'esbuild-node-externals';
 
 // Shared build config for both CommonJS and ESM formats
-const sharedConfig = {
+const sharedConfig: BuildOptions = {
   entryPoints: ['./src/index.ts'], // Entry point for your library
   bundle: true,
   minify: true,
@@ -14,19 +14,15 @@ const sharedConfig = {
 };
 
 // Build for CommonJS format
-esbuild
-  .build({
-    ...sharedConfig,
-    outdir: './dist/cjs', // Output directory for CommonJS
-    format: 'cjs', // CommonJS format
-  })
-  .catch(() => process.exit(1));
+build({
+  ...sharedConfig,
+  outdir: './dist/cjs', // Output directory for CommonJS
+  format: 'cjs', // CommonJS format
+}).catch(() => process.exit(1));
 
 // Build for ESModules format
-esbuild
-  .build({
-    ...sharedConfig,
-    outdir: './dist/esm', // Output directory for ESModules
-    format: 'esm', // ESModules format
-  })
-  .catch(() => process.exit(1));
+build({
+  ...sharedConfig,
+  outdir: './dist/esm', // Output directory for ESModules
+  format: 'esm', // ESModules format
+}).catch(() => process.exit(1));
